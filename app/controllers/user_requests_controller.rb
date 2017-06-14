be59eb04a2e545bc45f4dd5e4ee9d6ce3e8c607b
Erik Bonn <ebonn89@gmail.com>
2017-06-14 15:38:38 -0700

    notifications done

--- app/controllers/user_requests_controller.rb
+++ app/controllers/user_requests_controller.rb
@@ -1,9 +1,11 @@
 class UserRequestsController < ApplicationController
   def index
     advice_requests = current_user.advice_requests
+    puts current_user.advice_requests
     render json: current_user.advice_requests.to_json(include:
       [
-        { request_photos: { methods: :image, only: [:id] } }
+        { request_photos: { methods: :image, only: [:id] } },
+        { user: {only: [:username]} }
         # { advisors: { only: [:username, :id] } },
         # { comments: { only: [:body, :user_id, :request_photo_id]}}
       ])
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :profile_photos, only: [:new, :create, :show, :index]
   end
 
-
+get '/requests/advise', to: 'user_requests#index'
 
   post 'authenticate', to: 'authentication#authenticate'
 
