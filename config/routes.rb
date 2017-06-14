3c5b1b67ea943713e7d9ac0969de3972b0b104c9
Erik Bonn <ebonn89@gmail.com>
2017-06-14 14:48:17 -0700

    Change Auth to last 24 days, change route for notifications

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
     resources :requests, only: [:new, :create, :show, :index, :edit, :update] do
       resources :comments, only: [:new, :create, :show, :index, :edit, :update]
     end
-    resources :user_requests, only: [:index]
+    # resources :user_requests, only: [:index]
   end
 
   resources :users, only: [:show, :create, :new, :index] do
     resources :profile_photos
   end
 
-
+  get '/requests/advise', to: 'user_requests#index'
 
   post 'authenticate', to: 'authentication#authenticate'
 
